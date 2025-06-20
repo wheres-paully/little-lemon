d438444421fd11edacfae4a1bce17e8154d20120
Paully <paulus.grobler@gmail.com>
2025-06-20 12:33:36 +0100

    Adding LittleLemonDescription component and styling

--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -4,9 +4,11 @@ function Nav() {
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
           <li><a href="/menu">Menu</a></li>
-          <li><a href="/reservations">Reservations</a></li>
-          <li><a href="/about">About Us</a></li>
+          <li><a href="/reservation">Reservation</a></li>
+          <li><a href="/orderonline">Order Online</a></li>
+          <li><a href="/login">Login</a></li>
         </ul>
       </nav>
     );
